9a7e75c628280f094c43320fa5208d7b50700df3
Andrew Osborn <andrewosborn101@gmail.com>
2019-05-02 09:46:36 -0500

    initial commit

---
+++ client/build/precache-manifest.074f6fc6db6e53b10cc6ad082e8b240b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1831675fedbd3fc7dcd0",
+    "url": "/static/js/main.e2d76ee2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1831675fedbd3fc7dcd0",
+    "url": "/static/css/main.a1475568.chunk.css"
+  },
+  {
+    "revision": "e11cdabe90e868c146d55e79e19f809b",
+    "url": "/index.html"
+  }
+];
